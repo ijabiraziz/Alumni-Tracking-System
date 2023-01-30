cb99e6c23f001ecfde5f427b4ceb8089634334da
coding-with-ali <haxratali0@gmail.com>
2023-01-30 23:05:12 +0500

    added the bash script

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -1,6 +1,6 @@
 # build_files.sh
 python3.9 -m pip install virtualenv
 python3.9 -m venv env
-.\\env\\Bin\\activate
+./env/Bin/activate
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic
