96c2336e1f2655f66ad5f724620bcefc268ff8db
coding-with-ali <haxratali0@gmail.com>
2023-01-30 18:26:20 +0500

    make some changes

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -1,4 +1,2 @@
-echo" BUILD START"
-python -m pip install -r requirements.txt
-python manage.py collectstatic --noinput --clear
-echo" BUILD END" 
+python3.9 -m pip3 install -r requirements.txt
+python3.9 manage.py collectstatic --noinput --clear
