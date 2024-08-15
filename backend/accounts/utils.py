396da63b0e700cd01b3accc08c42ed71b812a587
Jabir Aziz <61976523+ijabiraziz@users.noreply.github.com>
2024-08-15 09:31:13 +0000

    re-structure utils.py

--- backend/accounts/utils.py
+++ backend/accounts/utils.py
@@ -1,19 +1,18 @@
-import pandas as pd
-from rest_framework_simplejwt.tokens import RefreshToken
+import uuid
 import random
 import string
 import hashlib
-import uuid
+import pandas as pd
+
+from rest_framework_simplejwt.tokens import RefreshToken
 
 def get_tokens_for_user(user):
     refresh = RefreshToken.for_user(user)
-
     return {
         'refresh': str(refresh),
         'access': str(refresh.access_token),
     }
     
-
 
 def xlxs_to_list_dict(file_path):    
     df = pd.read_excel(file_path)
@@ -21,12 +20,12 @@ def xlxs_to_list_dict(file_path):
     alumni_list=list(d)
     return alumni_list
 
+
 def get_random_string():
     # With combination of lower and upper case
     result_str = ''.join(random.choice(string.ascii_letters) for i in range(30))
     # print random string
     print(result_str)
-
 
 
 def generate_hash():
